aa1058fface912d80672a378b5e540b2dc571fcf
Administrador <administrador@SP16.local>
2016-06-15 19:55:20 -0300

    cargando datos en el detalle de publicacion

--- Obligatorio3/SingleEntryViewController.swift
+++ Obligatorio3/SingleEntryViewController.swift
@@ -11,9 +11,19 @@ import UIKit
 class SingleEntryViewController: UIViewController {
     
     var entry: Entry?
+    @IBOutlet weak var entryTitle: UILabel!
+    @IBOutlet weak var entryOrigin: UILabel!
+    @IBOutlet weak var entryDate: UILabel!
+    @IBOutlet weak var entryDestination: UILabel!
+    @IBOutlet weak var entryDescription: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        entryTitle.text = entry?.title
+        entryOrigin.text = entry?.origin
+        entryDestination.text = entry?.destination
+        entryDate.text = entry?.date
+        entryDescription.text = entry?.description
     }
 
 }
