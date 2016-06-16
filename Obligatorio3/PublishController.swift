2b49c28adfccbc6dc37f99cafed059589b8390c1
Administrador <mato23rey@gmail.com>
2016-06-15 21:13:41 -0300

    addpublicar

---
+++ Obligatorio3/PublishController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Obligatorio3
+//
+//  Created by Administrador on 9/6/16.
+//  Copyright © 2016 MICHO. All rights reserved.
+//
+
+import UIKit
+import IQKeyboardManagerSwift
+import MapKit
+
+
+class PublishController: UIViewController {
+    
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var txtDescription: UITextView!
+    var window: UIWindow?
+    
+    @IBAction func textFieldEditing(sender: UITextField) {
+        
+        let datePickerView:UIDatePicker = UIDatePicker()
+        
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        
+        sender.inputView = datePickerView
+        
+        //self.dateTextField.addTarget(self, action: #selector(ViewController.datePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
+        //http://stackoverflow.com/questions/28394933/how-do-i-check-when-a-uitextfield-changes
+        self.dateTextField.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    
+    
+    func datePickerValueChanged(sender:UIDatePicker) {
+        
+        let dateFormatter = NSDateFormatter()
+        
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        
+        self.dateTextField.text = dateFormatter.stringFromDate(sender.date)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.dateTextField.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+        
+        //Border of txtDescription
+        self.txtDescription!.layer.borderWidth = 1
+        self.txtDescription!.layer.borderColor = UIColor.grayColor().CGColor
+        
+        //Border of map
+        self.map!.layer.borderWidth = 1
+        self.map!.layer.borderColor = UIColor.grayColor().CGColor
+        
+        IQKeyboardManager.sharedManager().enable = true
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
